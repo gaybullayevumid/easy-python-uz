d76034474cf2152550c2e70e235823512071db75
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-28 21:08:31 +0500

    feat: dictionary tasks

--- 05-dictionary/tasks/tasks.py
+++ 05-dictionary/tasks/tasks.py
@@ -3,3 +3,10 @@ unsorted_dict = {'apple': 50, 'banana': 30, 'cherry': 20, 'date': 40}
 
 # Vazifa
 # Bu yerda kod yozing
+
+# task 2
+group_dict = {'a': 1, 'b': 2, 'c': 1, 'd': 2, 'e': 3}
+
+# Vazifa
+# Bu yerda kod yozing
+
