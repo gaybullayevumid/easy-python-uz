1cf1df28cd02b5890d3ecdf72e93f47e7803f0e1
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-28 16:48:29 +0500

    feat: practics set

--- 04-sets/main.py
+++ 04-sets/main.py
@@ -1,65 +1,73 @@
-# Bo'sh set yaratish
+# # Bo'sh set yaratish
 # my_set = set()
-# print(my_set)
 
-# Elementlar bilan set yaratish
-# my_set = {1, 2, 3, 4, 5, 4, 2}
-# print(my_set)
+# # Elementlar bilan set yaratish
 
-# son = 20
-# sonlar = [4,6,7,9,4,3]
+
 
 # my_set = {1, 2, 3, 4, 5}
-# my_set.update([6, 7, 8])
-# # my_set.add(6)
+# my_set.discard(9)
+# print(my_set)  # {1, 2, 4, 5}
+
+
 # my_set.remove(7)
-# my_set.discard(7)
+# print(my_set)  # {1, 2, 4, 5}
+
+# my_set.update([6, 7, 8])
+# print(my_set)  # {1, 2, 4, 5, 6, 7, 8}
+
+
+# my_set.add("hello world")
 # print(my_set)  # {1, 2, 3, 4, 5}
+
 
 # set1 = {1, 2, 3, 4, 6}
 # set2 = {3, 4, 5, 6}
-#
+
 # intersection_set = set1 & set2
 # print(intersection_set)  # Natija: {3, 4}
 
-# set1 = {1, 2, 3, 4}
+
+# set1 = {1, 2, 3, 4, 5}
 # set2 = {3, 4, 5, 6}
-# intersection_set = set1.intersection(set2)
-# print(intersection_set)
-# # print(set1.intersection(set2))  # {3, 4}
+# print(set1.intersection(set2))  # {3, 4}
 
 
 # set1 = {1, 2, 3, 4}
 # empty_set = set()
-#
+
 # intersection_set = set1 & empty_set
 # print(intersection_set)  # Natija: set()
 
+
+
 # set1 = {1, 2, 3, 4}
 # set2 = {3, 4, 5, 6}
-#
+
 # difference_set = set2 - set1
 # print(difference_set)  # Natija: {1, 2}
 
 
 # set1 = {1, 2, 3, 4}
 # set2 = {3, 4, 5, 6}
-#
+
 # difference_set = set2.difference(set1)
 # print(difference_set)  # Natija: {1, 2}
 
 
-
 # set1 = {1, 2, 3}
 # set2 = {3, 4, 5}
-#
+
 # union_set = set1 | set2
 # print(union_set)  # Natija: {1, 2, 3, 4, 5}
 
 
 # set1 = {1, 2, 3}
 # set2 = {3, 4, 5}
-#
+
 # union_set = set1.union(set2)
 # print(union_set)  # Natija: {1, 2, 3, 4, 5}
+
+
+
 
